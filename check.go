e201dda22edb9c135f88b9ba2774c69652cf52dc
Emil Rosendahl <emil.rosendahl@maxiv.lu.se>
2020-09-14 14:54:51 +0200

    Adds NotOK to check for errors

--- check.go
+++ check.go
@@ -26,6 +26,15 @@ func OK(tb testing.TB, err error) {
 	}
 }
 
+// NotOK fails the test if an err is nil.
+func NotOK(tb testing.TB, err error, msg string) {
+	if err == nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: expected error: %s\033[39m\n\n", filepath.Base(file), line, msg)
+		tb.FailNow()
+	}
+}
+
 // Equals fails the test if exp is not equal to act.
 func Equals(tb testing.TB, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
